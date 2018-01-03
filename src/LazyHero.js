6c33dee9412a16d19c380e572c1190160eedf1ab
Brahim Dahmani <dahmanibrahim7@gmail.com>
2018-01-03 23:28:00 +0100

    Pass contentWidth

--- src/LazyHero.js
+++ src/LazyHero.js
@@ -156,7 +156,7 @@ class LazyHero extends Component {
                     isCentered={this.props.isCentered}
                     opacity={this.props.opacity}
                 >
-                    {this.props.children && <div>{this.props.children}</div>}
+                    {this.props.children && <div className={this.props.contentWidth}>{this.props.children}</div>}
                 </Overlay>
             </Root>
         );
